8f2f0106b4fb82f7f7580c051ef1e63524ad8520
Zach <zdelrosario@outlook.com>
2021-01-20 13:14:47 -0500

    vectorize beam model

--- grama/models/cantilever_beam.py
+++ grama/models/cantilever_beam.py
@@ -7,31 +7,41 @@ from numpy import sqrt, array, Inf
 from scipy.stats import norm
 
 LENGTH = 100
-D_MAX  = 2.2535
+D_MAX = 2.2535
 
-MU_H   = 500.
-MU_V   = 1000.
-MU_E   = 2.9e7
-MU_Y   = 40000.
+MU_H = 500.0
+MU_V = 1000.0
+MU_E = 2.9e7
+MU_Y = 40000.0
 
-TAU_H  = 100.
-TAU_V  = 100.
-TAU_E  = 1.45e6
-TAU_Y  = 2000.
+TAU_H = 100.0
+TAU_V = 100.0
+TAU_E = 1.45e6
+TAU_Y = 2000.0
 
-def function_area(x):
-    w, t = x
-    return w * t
 
-def function_stress(x):
-    w, t, H, V, E, Y = x
-    return (Y - 600 * V / w / t**2 - 600 * H / w**2 / t) / MU_Y
+def function_area(df):
+    return gr.df_make(c_area=df.w * df.t)
 
-def function_displacement(x):
-    w, t, H, V, E, Y = x
-    return D_MAX - np.float64(4) * LENGTH**3 / E / w / t * sqrt(
-        V**2 / t**4 + H**2 / w**4
+
+def function_stress(df):
+    return gr.df_make(
+        g_stress=(df.Y - 600 * df.V / df.w / df.t ** 2 - 600 * df.H / df.w ** 2 / df.t)
+        / MU_Y
     )
+
+
+def function_displacement(df):
+    return gr.df_make(
+        g_disp=D_MAX
+        - np.float64(4)
+        * LENGTH ** 3
+        / df.E
+        / df.w
+        / df.t
+        * sqrt(df.V ** 2 / df.t ** 4 + df.H ** 2 / df.w ** 4)
+    )
+
 
 def make_cantilever_beam():
     """Cantilever beam
@@ -68,38 +78,37 @@ def make_cantilever_beam():
 
     """
 
-    md = gr.Model(name = "Cantilever Beam") >> \
-         gr.cp_function(
-             fun=function_area,
-             var=["w", "t"],
-             out=["c_area"],
-             name="cross-sectional area",
-             runtime=1.717e-7
-         ) >> \
-         gr.cp_function(
-             fun=function_stress,
-             var=["w", "t", "H", "V", "E", "Y"],
-             out=["g_stress"],
-             name="limit state: stress",
-             runtime=8.88e-7
-         ) >> \
-         gr.cp_function(
-             fun=function_displacement,
-             var=["w", "t", "H", "V", "E", "Y"],
-             out=["g_disp"],
-             name="limit state: displacement",
-             runtime=3.97e-6
-         ) >> \
-         gr.cp_bounds(
-             w=(2, 4),
-             t=(2, 4)
-         ) >> \
-         gr.cp_marginals(
-             H={"dist": "norm", "loc": MU_H, "scale": TAU_H, "sign": +1},
-             V={"dist": "norm", "loc": MU_V, "scale": TAU_V, "sign": +1},
-             E={"dist": "norm", "loc": MU_E, "scale": TAU_E, "sign":  0},
-             Y={"dist": "norm", "loc": MU_Y, "scale": TAU_Y, "sign": -1}
-         ) >> \
-         gr.cp_copula_independence()
+    md = (
+        gr.Model(name="Cantilever Beam")
+        >> gr.cp_vec_function(
+            fun=function_area,
+            var=["w", "t"],
+            out=["c_area"],
+            name="cross-sectional area",
+            runtime=1.717e-7,
+        )
+        >> gr.cp_vec_function(
+            fun=function_stress,
+            var=["w", "t", "H", "V", "E", "Y"],
+            out=["g_stress"],
+            name="limit state: stress",
+            runtime=8.88e-7,
+        )
+        >> gr.cp_vec_function(
+            fun=function_displacement,
+            var=["w", "t", "H", "V", "E", "Y"],
+            out=["g_disp"],
+            name="limit state: displacement",
+            runtime=3.97e-6,
+        )
+        >> gr.cp_bounds(w=(2, 4), t=(2, 4))
+        >> gr.cp_marginals(
+            H={"dist": "norm", "loc": MU_H, "scale": TAU_H, "sign": +1},
+            V={"dist": "norm", "loc": MU_V, "scale": TAU_V, "sign": +1},
+            E={"dist": "norm", "loc": MU_E, "scale": TAU_E, "sign": 0},
+            Y={"dist": "norm", "loc": MU_Y, "scale": TAU_Y, "sign": -1},
+        )
+        >> gr.cp_copula_independence()
+    )
 
     return md
